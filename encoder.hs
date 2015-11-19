9ccffd64dd60cb6857dc55ebaa432c5ecc9d0772
hobbit644 <olliemathews@live.com>
2015-11-19 15:54:08 +0000

    Initial commit

---
+++ encoder.hs
@@ -0,0 +1,15 @@
+import System.IO
+import Data.Char
+
+main = do hSetBuffering stdout NoBuffering
+          putStrLn "What encoding should be used?"
+          name <- getLine
+          contents <- readFile name
+          putStrLn "What file should be encoded?"
+          textName <- getLine
+          text <- readFile textName
+          let rstrip = reverse.dropWhile isSpace.reverse
+              readLister = read :: String -> [(Char,String)]
+              encode x = snd $ head $ filter ((==x).fst) (readLister contents)
+              encode :: Char -> String
+          print $ concat $ map encode (rstrip text) 
